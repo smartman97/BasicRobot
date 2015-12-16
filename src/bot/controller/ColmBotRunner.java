60da60ea40a72c202498e25b2fbd630aea1596ba
Colm Laro <smartman97@gmail.com>
2015-12-16 08:47:58 -0700

    Created a ledges project

---
+++ src/bot/controller/ColmBotRunner.java
@@ -0,0 +1,12 @@
+package bot.controller;
+
+public class ColmBotRunner
+{
+
+	public static void main(String[] args)
+	{
+		BotController myBotController = new BotController();
+		myBotController.start();
+	}
+
+}
